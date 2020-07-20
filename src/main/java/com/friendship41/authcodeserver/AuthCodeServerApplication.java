05f7d12fc5343d003d500260157f4ea5c754e867
ht-chchoi <chchoi@hdtel.co.kr>
2020-07-20 17:47:43 +0900

    TODO 리스트 작성

--- src/main/java/com/friendship41/authcodeserver/AuthCodeServerApplication.java
+++ src/main/java/com/friendship41/authcodeserver/AuthCodeServerApplication.java
@@ -9,5 +9,7 @@ public class AuthCodeServerApplication {
   public static void main(String[] args) {
     SpringApplication.run(AuthCodeServerApplication.class, args);
   }
-
+  // TODO: 로그파일 잘 떨어지게 만들어야함
+  // TODO: mem_no를 이용한 정보보기 api
+  // TODO: Error 페이지 구성
 }
