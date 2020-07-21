8be553c8257d8c81a65a79809ad4c439ee137625
friendship41 <stage41@nate.com>
2020-07-21 23:03:40 +0900

    로그 최종 설정...이길

--- src/main/java/com/friendship41/authcodeserver/AuthCodeServerApplication.java
+++ src/main/java/com/friendship41/authcodeserver/AuthCodeServerApplication.java
@@ -7,7 +7,6 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @SpringBootApplication
 public class AuthCodeServerApplication {
-
   public static void main(String[] args) {
     SpringApplication.run(AuthCodeServerApplication.class, args);
   }
